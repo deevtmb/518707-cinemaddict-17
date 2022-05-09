bcac1d7bb66aeecff060756650e470718a10f1e5
Andrei <l2.ls@yandex.ru>
2022-05-09 17:02:44 +0300

    Отображение количества фильмов для фильтров

--- src/view/filter-view.js
+++ src/view/filter-view.js
@@ -1,16 +1,29 @@
 import AbstractView from '../framework/view/abstract-view.js';
+import {getFilteredFilms} from '../mock/filters.js';
 
-const createFilterTemplate = () => (
-  `<nav class="main-navigation">
-    <a href="#all" class="main-navigation__item main-navigation__item--active">All movies</a>
-    <a href="#watchlist" class="main-navigation__item">Watchlist <span class="main-navigation__item-count">13</span></a>
-    <a href="#history" class="main-navigation__item">History <span class="main-navigation__item-count">4</span></a>
-    <a href="#favorites" class="main-navigation__item">Favorites <span class="main-navigation__item-count">8</span></a>
-  </nav>`
-);
+const createFilterTemplate = (films) => {
+  const filteredFilms = getFilteredFilms(films);
+
+  return (
+    `<nav class="main-navigation">
+      <a href="#all" class="main-navigation__item main-navigation__item--active">All movies</a>
+      <a href="#watchlist" class="main-navigation__item">Watchlist <span class="main-navigation__item-count">${filteredFilms.watchlist.length}</span></a>
+      <a href="#history" class="main-navigation__item">History <span class="main-navigation__item-count">${filteredFilms.history.length}</span></a>
+      <a href="#favorites" class="main-navigation__item">Favorites <span class="main-navigation__item-count">${filteredFilms.favorite.length}</span></a>
+    </nav>`
+  );
+};
 
 export default class FilterView extends AbstractView {
+  #films = null;
+
+  constructor (films) {
+    super();
+    this.#films = films;
+
+  }
+
   get template() {
-    return createFilterTemplate();
+    return createFilterTemplate(this.#films);
   }
 }
